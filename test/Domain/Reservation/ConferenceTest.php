8429667d08edacafe41868764aea6367e9d2f49d
Marek Makowski <marek.makowski@e-surf.pl>
2017-09-01 10:49:12 +0200

    ex1

--- test/Domain/Reservation/ConferenceTest.php
+++ test/Domain/Reservation/ConferenceTest.php
@@ -9,8 +9,13 @@ class ConferenceTest extends TestCase
     /**
      * @todo: remove it
      */
-    public function test_example_name()
+    public function test_make_reservation_for_order_with_reservation_already()
     {
         $this->markTestSkipped();
     }
-}
+    
+    public function test_make_reservation_for_more_seats_than_avilability()
+    {
+        $this->markTestSkipped();
+    }
+ }
