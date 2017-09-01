109aac3c9049e136b44670f6bb1956f575a0cd65
patwoj98 <patwoj98@gmail.com>
2017-09-01 10:48:01 +0200

    Task 1: Reservation test

--- test/Domain/Reservation/ConferenceTest.php
+++ test/Domain/Reservation/ConferenceTest.php
@@ -3,14 +3,45 @@
 namespace RstGroup\ConferenceSystem\Domain\Reservation\Test;
 
 use PHPUnit\Framework\TestCase;
+use RstGroup\ConferenceSystem\Domain\Reservation\OrderId;
 
 class ConferenceTest extends TestCase
 {
-    /**
-     * @todo: remove it
-     */
-    public function test_example_name()
-    {
-        $this->markTestSkipped();
+
+    public function makeReservationForOrder(){
+        //czy ordernumber jest int,
+        //dostępność miejsc
+        //ilosć miejsc
+
+        //sprawdź poprawność orderId
+    }
+
+    public function isOrderIdObjectOrderId(){
+        //oczekujemy zwrócenia obiektu typu OrderId
+    }
+
+    public function isSeatsObjectSeatsCollection(){
+        //oczekujemy zwrócenia obiektu typu seatsCollection
+    }
+
+    public function reservationDoesExist(){
+        //przerwanie
+        //taka rezerwacja już istnieje
+    }
+
+    public function isReservationCorrectlyInserted(){
+        //sprawdzanie czy po zakończeniu rzeczywiście dodano rezerwację
+    }
+
+    public function tooLessSitsQuanity(){
+        //oczekiwanie większej lub równej ilości miejsc
+    }
+
+    public function notAddedIntoWaitList(){
+        //oczekiwanie znalezienie numeru rezerwacji w liście
+    }
+
+    public function isReturnNull(){
+        //oczekiwanie zwrócenia nulla przy poprawnych danych wejściowych
     }
 }
