53cb4338c3c64dcb8a8f8ec2dcaddbd04aadc51c
Patryk Grudniewski <patgrudniewski@gmail.com>
2017-09-01 10:48:14 +0200

    Remove example test

--- test/Domain/Reservation/ConferenceTest.php
+++ test/Domain/Reservation/ConferenceTest.php
@@ -6,14 +6,6 @@ use PHPUnit\Framework\TestCase;
 
 class ConferenceTest extends TestCase
 {
-    /**
-     * @todo: remove it
-     */
-    public function test_example_name()
-    {
-        $this->markTestSkipped();
-    }
-
     public function testReservationExistanceChecked()
     {
         $this->markTestSkipped();
