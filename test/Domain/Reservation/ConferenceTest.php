9f31350f289715c3da21745266695eb42ef93023
Karol Kreft <kkreft@accesto.pl>
2017-09-01 11:48:24 +0200

    remove example test

--- test/Domain/Reservation/ConferenceTest.php
+++ test/Domain/Reservation/ConferenceTest.php
@@ -6,14 +6,6 @@ use PHPUnit\Framework\TestCase;
 
 class ConferenceTest extends TestCase
 {
-    /**
-     * @todo: remove it
-     */
-    public function test_example_name()
-    {
-        $this->markTestSkipped();
-    }
-
     public function test_it_throw_exception_when_reservation_does_not_have_order() {}
 
     public function test_it_increment_available_seats_quantity() {}
@@ -23,6 +15,4 @@ class ConferenceTest extends TestCase
     public function test_it_does_not_move_reservation_from_waitlist_when_seats_are_not_available() {}
 
     public function test_it_move_reservation_from_waitlist_when_seats_are_available() {}
-
-
 }
