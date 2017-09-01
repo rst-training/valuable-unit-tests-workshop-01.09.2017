8086bd4c331b4edb3e2c842b04a378b4241b523f
Marcin Klepacki <mar.klepacki@gmail.com>
2017-09-01 10:47:58 +0200

    makeReservationForOrder tests

--- test/Domain/Reservation/ConferenceTest.php
+++ test/Domain/Reservation/ConferenceTest.php
@@ -6,11 +6,55 @@ use PHPUnit\Framework\TestCase;
 
 class ConferenceTest extends TestCase
 {
-    /**
-     * @todo: remove it
-     */
-    public function test_example_name()
+    public function test_make_reservation_reservation_exists()
     {
         $this->markTestSkipped();
     }
+
+    public function test_make_reservation_reservation_waitlist()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function test_make_reservation_reservation_completed()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function test_make_reservation_invalid_order_id()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function test_make_reservation_empty_seat_collection()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function test_make_reservation_multiple_seat_types_with_one_seat_type_exceeded_exceeded_first()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function test_make_reservation_multiple_seat_types_with_one_seat_type_exceeded_exceeded_last()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function test_make_reservation_multiple_seat_types_with_one_seat_type_exceeded_exceeded_middle()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function test_make_reservation_multiple_seat_types_with_all_seat_types_exceeded()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function test_make_reservation_multiple_seat_types_with_all_seat_types_quantity_valid()
+    {
+        $this->markTestSkipped();
+    }
+
+
 }
