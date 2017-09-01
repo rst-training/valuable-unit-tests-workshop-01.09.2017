1f833cd286f282477563168ebf73109c223fa0d4
Patryk Grudniewski <patgrudniewski@gmail.com>
2017-09-01 10:46:16 +0200

    Methods for testing canceling reservation for order

--- test/Domain/Reservation/ConferenceTest.php
+++ test/Domain/Reservation/ConferenceTest.php
@@ -13,4 +13,35 @@ class ConferenceTest extends TestCase
     {
         $this->markTestSkipped();
     }
+
+    public function testReservationExistanceChecked()
+    {
+        $this->markTestSkipped();
+    }
+
+    /**
+     * @todo: remember about wait list checking here
+     */
+    public function testSeatsQuantityIncremented()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testReservationRemoval()
+    {
+        $this->markTestSkipped();
+    }
+
+    /**
+     * @todo: remember case when any item from wait list cannot be moved
+     */
+    public function testWaitListItemMoved()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testWaitListReservationAdded()
+    {
+        $this->markTestSkipped();
+    }
 }
