68368957b17e6fac4de709faa7a9441272177cfd
MATI <wiprzemek9@tlen.pl>
2017-09-01 10:47:02 +0200

    Cancel test methods

--- test/Domain/Reservation/ConferenceTest.php
+++ test/Domain/Reservation/ConferenceTest.php
@@ -4,13 +4,13 @@ namespace RstGroup\ConferenceSystem\Domain\Reservation\Test;
 
 use PHPUnit\Framework\TestCase;
 
-class ConferenceTest extends TestCase
-{
-    /**
-     * @todo: remove it
-     */
-    public function test_example_name()
-    {
-        $this->markTestSkipped();
+class ConferenceTest extends TestCase{
+    public function reservation_does_not_exists_expected(){
+    }
+    public function seats_availability_increment_quantity(){
+    }
+    public function reservation_removed_correctly(){
+    }
+    public function seats_can_be_moved(){
     }
 }
